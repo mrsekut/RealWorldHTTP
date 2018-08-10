ba3100792e189eef4286e6daac997049a74f6e8b
mrsekut <k.cloudspider@gmail.com>
2018-08-11 02:55:24 +0900

    1.1.1-1.5.1

---
+++ 01/1.1.1.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(string(dump))
+	fmt.Fprintf(w, "<html><body>hello world</body></html>")
+}
+
+func main() {
+	var httpServer http.Server
+	http.HandleFunc("/", handler)
+	log.Println("start http listening :18888")
+	httpServer.Addr = ":18888"
+	log.Println(httpServer.ListenAndServe())
+}
+
+/*
+通信内容をそのままコンソールに表示する
+handler関数は、httputil.DumpRequest関数を使ってリクエストに含まれる情報をテキストとして出力
+
+
+
+*/
