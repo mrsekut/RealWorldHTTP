92586277dccc855183a4d7323d21676a886f5eac
mrsekut <k.cloudspider@gmail.com>
2018-08-29 02:22:16 +0900

    [Fix]多重化まで

---
+++ myproduct/https_server/http_google.go
@@ -0,0 +1,17 @@
+// HTTPプロトコルバージョンを確認
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("http://google.com") // HTTP/1.1
+	// resp, err := http.Get("https://google.com") // HTTP/2.0
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	fmt.Printf("Protocol Version: %s\n", resp.Proto)
+}
