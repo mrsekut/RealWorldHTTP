345b818f08bfca6f81a6777d9db69e448bac1dc8
mrsekut <k.cloudspider@gmail.com>
2018-09-06 01:30:05 +0900

    2018-9-6

---
+++ 11/11.6.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"golang/org/x/oauth2/github"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/skratchbot/open-golang/open"
+	"golang.org/x/oauth2"
+)
+
+var clientID = ""
+var clientSecret = ""
+var redirectURL = "htttps://localhost:18888"
+var state = "your state"
+
+func main() {
+	conf := &oauth2.Config{
+		clientID:     clientID,
+		ClientSecret: clientSecret,
+		Scopes:       []string{"user:email", "gist"},
+		Endpoint:     github.Endpoint,
+	}
+
+	var token *oauth2.Token
+
+	file, err := os.Open("access_token.json")
+	if os.IsNotExist(err) {
+		url := conf.AuthCodeURL(state, oauth.AccessTypeOnline)
+
+		code := make(chan string)
+		var server *http.Server
+		server = &http.Server{
+			Addr: ":18888",
+			Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "text/html")
+				io.WriteString(w, "<html><script>window.open('about:blank', '_self').close()</script></html>")
+				w.(hettp.Flusher).Flush()
+				code <- r.URL.Query().Get("code")
+				server.Shutdown(context.Background())
+			}),
+		}
+
+		open.Start(url)
+
+		token, err = conf.Exchange(oauth2.NoContext, <-code)
+		if err != nil {
+			panic(err)
+		}
+
+		file, err := os.Create("access_token.json")
+		if err != nil {
+			panic(err)
+		}
+		json.NewEncoder(file).Encode(token)
+	} else if err == nil {
+		token = &oauth2.Token{}
+		json.NewDecoder(file).Decode(token)
+	} else {
+		panic(err)
+	}
+	client := oauth2.NewClient(oauth2.NoContext, conf.TokenSource(oauth2.NoContext, token))
+}
